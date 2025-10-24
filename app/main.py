6409882cde873a09162c6e89619ccc3e5b2c3782
Kami <josephkiarie561@gmail.com>
2025-10-24 09:36:54 +0300

    loading the model

--- app/main.py
+++ app/main.py
@@ -5,4 +5,20 @@ import pandas as pd
 import numpy as np
 import pickle
 from datetime import datetime
+from pathlib import path
 import matplotlib.pyplot as plt
+
+MODEL_PATH = path("../models/energy_model.sav")
+
+# loading the model
+@st.cache_resource
+def load_model():
+    if not MODEL_PATH.exists():
+        raise FileNotFoundError(f"model file not found at {MODEL_PATH}")
+    
+    with open(MODEL_PATH, "rb") as f:
+        model = pickle.load(f)
+
+    return model
+
+model = load_model()
